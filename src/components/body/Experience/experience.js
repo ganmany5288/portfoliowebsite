05efaedbd7336772e38c87a67485763c0d4c0bb7
James Tang <jamestang@uvic.ca>
2022-08-06 14:09:41 -0700

    Experience container done

--- src/components/body/Experience/experience.js
+++ src/components/body/Experience/experience.js
@@ -3,18 +3,22 @@ import './experience.css'
 
 function Experience(){
     return(
-        <div className='experience-container'>
-            <div className='word-container'>
-                <h1>List of Experiences</h1>
+        <div className='exp-container-container'>
+            <div className='picture-container'>
+                {/* <h1>Hello There</h1> */}
             </div>
-            <div className='list-of-experiences'>
-                <div className='experience-grid'>
-                    <div className='experience'></div>
-                    <div className='experience'></div>
-                    <div className='experience'></div>
-                    <div className='experience'></div>
-                    <div className='experience'></div>
-                    {/* <div className='experience'></div> */}
+            <div className='experience-container'>
+                <div className='word-container'>
+                    <h1>Work Experiences</h1>
+                </div>
+                <div className='list-of-experiences'>
+                    <div className='experience-grid'>
+                        <div className='experience'></div>
+                        <div className='experience'></div>
+                        <div className='experience'></div>
+                        <div className='experience'></div>
+                        <div className='experience'></div>
+                    </div>
                 </div>
             </div>
         </div>
