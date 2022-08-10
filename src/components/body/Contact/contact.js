12dbe5bcbc33de1e729f9ab27ad7122ee807309d
James Tang <jamestang@uvic.ca>
2022-08-10 10:58:14 -0700

    Contact changes

--- src/components/body/Contact/contact.js
+++ src/components/body/Contact/contact.js
@@ -4,8 +4,31 @@ import './contact.css'
 function Contact(){
     return(
         <div className='contact-container'>
-            <h1>You guys are welcome to contact me or something...</h1>
+            <div className='contact-info'>
+                <h1>NihaoMa</h1>
+                <div className='contact-card'>
+                    <div>
+                        <h1>James Tang</h1>
+                        <div className='contacts'>
+                            <p>rhsjamestang@gmail.com</p>
+                            <p>604-537-3284</p>
+                            <p>www.reallygreatsite.com</p>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className='test'>
+                        <h2>Find Resume and Cover Letter here</h2>
+                        <p>Socials!</p>
+                        <p>Linkedin</p>
+                        <p>Instagram</p>
+                        <p>Discord</p>
+                        <p>Twitter</p>
+                        <p>Facebook</p>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
 
