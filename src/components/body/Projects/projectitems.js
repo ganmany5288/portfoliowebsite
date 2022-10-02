b707ea45a36223730b1ab182692256d1ed760b47
ganmany5288 <jamestang@uvic.ca>
2022-10-02 10:03:32 -0700

    Projects Section rework part 2

--- src/components/body/Projects/projectitems.js
+++ src/components/body/Projects/projectitems.js
@@ -6,7 +6,7 @@ function Projectitems(props) {
     return(
         <div className='project-items'>
             <a href='https://www.lttstore.com' className='project-link'>
-                <img src="https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502" className="img-responsive"></img>
+                <img src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute.jpg" className="img-responsive"></img>
             </a>
             <div className='project-caption'>
                 <h4>{h4t}</h4>
