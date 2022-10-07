5a21ef26bb86c7c87afb8c92f2ba95a097ff8377
ganmany5288 <jamestang@uvic.ca>
2022-10-06 21:59:30 -0700

    Professional Experience Card design part 1

--- src/components/body/Experience/experience.js
+++ src/components/body/Experience/experience.js
@@ -12,16 +12,30 @@ function Experience(){
                     <div className='page-header'>
                         <h2 className='exp-header-text'>Professional Experiences</h2>
                     </div>
-                    <div className='experience-container'>
-                        {/* <h1>Here is some container information</h1> */}
-                        <div className='exp-flex'>
-                            <div className='exp-left'>
-                                {/* <p>hello there</p> */}
+                    <div className='flex'>
+                        <div className='row-page3-top center-small'>
+                            <div className='row-display'>
+                                <div className='card column-page2 card-l'>
+                                    <div>
+                                        <h1 class='card-title chain-same-line'>NewMode - Quality Assurance Analyst</h1>
+                                        <h1 class='card-sub'>XXXX to XXXX</h1>
+                                        <h1 class='card-desc'>This is my job description</h1>
+                                    </div>
+                                    <div className='portion-bottom'>
+
+                                    </div>
+                                </div>
+                                <div className='card column-page2 card-r'>
+                                    <div>
+                                        <h1 class='card-title chain-same-line'>NewMode - Quality Assurance Analyst</h1>
+                                        <h1 class='card-sub'>XXXX to XXXX</h1>
+                                        <h1 class='card-desc'>This is my job description</h1>
+                                    </div>
+                                    <div className='portion-bottom'>
+
+                                    </div>
+                                </div>
                             </div>
-                            <div className='exp-right'></div>
-                        </div>
-                        <div className='exp-flex'>
-                            <div className='exp-sing'></div>
                         </div>
                     </div>
                 </div>
