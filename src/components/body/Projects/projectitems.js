126bd3e62b245539af440f2d912d414c7d4136b1
James Tang <52274101+ganmany5288@users.noreply.github.com>
2023-12-20 16:33:07 -0800

    Project Section Updated

--- src/components/body/Projects/projectitems.js
+++ src/components/body/Projects/projectitems.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './project2.css'
 
+
+
 function Projectitems(props) {
-    const {h4t, pt} = props
+    const {link, img, h4t, pt} = props
     return(
         <div className='project-items'>
-            <a href='https://www.lttstore.com' className='project-link'>
-                <img src="https://cdn.freecodecamp.org/testable-projects-fcc/images/tribute.jpg" className="img-responsive"></img>
+            <a href={link} className='project-link'>
+                <img src={img} className="img-responsive" alt='an image'></img>
             </a>
             <div className='project-caption'>
                 <h4>{h4t}</h4>
--- src/components/body/Projects/projects2.js
+++ src/components/body/Projects/projects2.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Projectitems from './projectitems'
 import './project2.css'
-
+import ludum54 from '../../../images/ludum54.png'
+import test from '../../../images/test.png'
 
 function Projects2() {
     return(
@@ -13,12 +14,12 @@ function Projects2() {
                     </div>
                 </div>
                 <div className='projects-grid'>
-                    <Projectitems h4t={"Round Icons"} pt={"Graphic Design"}/>
-                    <Projectitems h4t={"Linus"} pt={"Tech Tips"}/>
-                    <Projectitems h4t={"Linus"} pt={"Drop Tips"}/>
+                    <Projectitems link={"https://ldjam.com/events/ludum-dare/54/braincell-limited"} img={ludum54} h4t={"GameJam - Ludum Dare 54"} pt={"<Link to Game>"}/>
+                    <Projectitems link={"https://hummusportfolio.netlify.app/"} img={test} h4t={"This site"} pt={"<Link to Site>"}/>
+                    {/* <Projectitems h4t={"Linus"} pt={"Drop Tips"}/>
                     <Projectitems h4t={"Linus"} pt={"Cat Tips"}/>
                     <Projectitems h4t={"Linus"} pt={"Cat Tips"}/>
-                    <Projectitems h4t={"Linus"} pt={"Cat Tips"}/>
+                    <Projectitems h4t={"Linus"} pt={"Cat Tips"}/> */}
                 </div>
             </div>
         </section>
